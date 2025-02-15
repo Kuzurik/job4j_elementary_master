d668ef33e2283faa4d90034b2bf47219fe3a205d
Lenovo <vorota-24@bk.ru>
2025-02-15 13:29:13 +0300

    5. Конвертер валюты [#41583 #111942]

---
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,16 @@
+package ru.job4j.converter;
+
+public class Converter {
+    public static float rubleToEuro(float value) {
+        return value / 70;
+    }
+
+    public static float rubleToDollar(float value) {
+        return value / 60;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("140 rubles are " + rubleToEuro(140) + " euro.");
+        System.out.println("180 rubles are " + rubleToDollar(180) + " dollars.");
+    }
+}
