cca268b655f7b4c0f1d7e7f0ff3b82f25f0c69e6
kuzurik <vorota-24@bk.ru>
2025-02-26 16:45:21 +0300

    3. Тесты для определения расстояния между точками. [#159459 #161969]

---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void when00to20then2() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double output = Point.distance(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when03to31then3Dot60() {
+        double expected = 3.60;
+        int x1 = 0;
+        int y1 = 3;
+        int x2 = 3;
+        int y2 = 1;
+        double output = Point.distance(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when1Minus3to31then4Dot47() {
+        double expected = 4.47;
+        int x1 = 1;
+        int y1 = -3;
+        int x2 = 3;
+        int y2 = 1;
+        double output = Point.distance(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when3Minus6to30then6() {
+        double expected = 6;
+        int x1 = 3;
+        int y1 = -6;
+        int x2 = 3;
+        int y2 = 0;
+        double output = Point.distance(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+}
