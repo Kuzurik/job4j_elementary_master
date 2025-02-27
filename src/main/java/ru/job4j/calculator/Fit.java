31221ec7393cce2049e0eafeed9e25fb8b5a0448
kuzurik <vorota-24@bk.ru>
2025-02-27 13:43:51 +0300

    GPT. Нейросети спешат на помощь. [#505156 #534088]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -1,16 +1,20 @@
 package ru.job4j.calculator;
 
 public class Fit {
-    public static double manWeight(short height) {
-        return (height - 100) * 1.15;
-    }
-
-    public static double womanWeight(short height) {
-        return (height - 110) * 1.15;
+    // Универсальный метод для расчета веса
+    public static double calculateWeight(short height, double baseValue, double coefficient) {
+        return (height - baseValue) * coefficient;
     }
 
     public static void main(String[] args) {
-        System.out.println("Man 187 is " + manWeight((short) 187));
-        System.out.println("Woman 160 is " + womanWeight((short) 160));
+        short heightMan = 187;
+        short heightWoman = 170;
+
+        // Используем универсальный метод для мужчин и женщин
+        double manWeight = Fit.calculateWeight(heightMan, 100, 1.15);
+        System.out.println("Man 187 is " + manWeight);
+
+        double womanWeight = Fit.calculateWeight(heightWoman, 110, 1.15);
+        System.out.println("Woman 170 is " + womanWeight);
     }
 }
--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -6,11 +6,15 @@ import static org.assertj.core.api.Assertions.*;
 
 class FitTest {
 
+    public static final int BASE_VALUE_MAN = 100;
+    public static final int BASE_VALUE_WOMAN = 110;
+    public static final double COEFFICIENT = 1.15;
+
     @Test
     void whenMan180Then92() {
         short input = 180;
         double expected = 92;
-        double output = Fit.manWeight(input);
+        double output = Fit.calculateWeight(input, BASE_VALUE_MAN, COEFFICIENT);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
@@ -18,7 +22,7 @@ class FitTest {
     void whenWoman170Then69() {
         short input = 170;
         double expected = 69;
-        double output = Fit.womanWeight(input);
+        double output = Fit.calculateWeight(input, BASE_VALUE_WOMAN, COEFFICIENT);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
